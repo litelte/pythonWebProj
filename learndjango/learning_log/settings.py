0f4e355d9e263e07ff7e411140473c4de145a62c
litelte <litecaoxzero98@foxmail.com>
2022-05-18 18:31:21 +0800

    创建应用程序

--- learndjango/learning_log/settings.py
+++ learndjango/learning_log/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # 添加我的应用程序
+    'learning_logs',
+    # 默认添加的应用程序
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ learndjango/learning_logs/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+from .models import Entry, Topic
+admin.site.register(Topic)
+admin.site.register(Entry)
