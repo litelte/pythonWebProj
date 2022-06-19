678f1e56a4743dcc17cb7746a85ca7bd72fd82f4
litelte <litecaoxzero98@foxmail.com>
2022-06-19 15:23:36 +0800

    重新review一下项目

--- learn_django2/hello_world_Review/review_hello_world/urls.py
+++ learn_django2/hello_world_Review/review_hello_world/urls.py
@@ -1,4 +1,4 @@
-"""hello_world URL Configuration
+"""review_hello_world URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
